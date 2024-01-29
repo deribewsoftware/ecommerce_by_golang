9f13392b09918e4d423b936fb7d6a34098f7cb2c
deribew <deribewsoftware@gmail.com>
2024-01-29 21:31:04 +0300

    database  added

---
+++ database/database_setup.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSet() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://deribewtech:golang_rest@cluster0.sklnweb.mongodb.net/"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx,cancel:context.WithTimeout(context.Background(),10*time.Second)
+ defer cancel()
+ err=client.Connect(ctx)
+ if err != nil {
+	log.Fatal(err)
+ }
+ err=client.Ping(context.TODO(),nil)
+ if err != nil {
+	log.Println("failed to connect to MongoDB:(")
+	return nil
+ }
+
+}
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+
+}
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+
+}
